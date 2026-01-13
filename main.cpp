843312724c9814f57a117613f0a5c84c7097d63f
Mark Mahoney <markm208@gmail.com>
2026-01-13 11:18:28 -0600

    Add main.cpp with course roster functions

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void printNameA_H();
+void printNameI_P();
+void printNameQ_Z();
+
+int main()
+{
+    cout<<"Course Roster: "<<endl;
+  
+    printNameA_H();
+    printNameI_P();
+    printNameQ_Z();
+
+    return 0;
+}
+//--
+void printNameA_H()
+{
+    cout<<"None"<<endl;
+}
+//--
+void printNameI_P()
+{
+    cout<<"None"<<endl;
+}
+//--
+void printNameQ_Z()
+{
+    cout<<"None"<<endl;
+}
